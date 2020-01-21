19ab12cd5ba2d4ac675861b0b4d6f536e476885d
Elisabeth <elisabethearley@gmail.com>
2020-01-21 11:29:22 -0500

    used wrong syntax as Link, changed Link in SavedList to correct syntax ' {}, Saved movie title links now functional. Two Stretch Goals completed.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,7 +5,7 @@ const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <Link to='/movies/:id' className="saved-movie">{movie.title}</Link>
+      <Link to={ `/movies/${movie.id}` } className="saved-movie">{movie.title}</Link>
     ))}
     <Link to='/' className="home-button">Home</Link>
   </div>
